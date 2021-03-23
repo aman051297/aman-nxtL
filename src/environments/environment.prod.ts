f7680ef1575acdce3597a4d68b4792f42c415d05
Aman <amansaxena781@gmail.com>
2021-03-23 17:51:55 +0530

    add MAPBOX_TOKEN in environments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
-  production: true,
-  BACKEND_HOST: 'http://103.155.146.44:8087',
-  SITE_URL: 'http://localhost:4200/'
+	production: true,
+	BACKEND_HOST: "http://103.155.146.44:8087",
+	SITE_URL: "http://localhost:4200/",
+	MAPBOX_TOKEN: "pk.eyJ1IjoiYW1hbjA1MTI5NyIsImEiOiJja21sbnY4M2EwN29zMndxc2Z4ajBjeWxqIn0.jXJe2w2wgn8EVWmEJP15xQ",
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,8 @@
 export const environment = {
    production: false,
   BACKEND_HOST: 'http://103.155.146.44:8087/',
-  SITE_URL: 'http://localhost:4200/'
+  SITE_URL: 'http://localhost:4200/',
+  MAPBOX_TOKEN:'pk.eyJ1IjoiYW1hbjA1MTI5NyIsImEiOiJja21sbnY4M2EwN29zMndxc2Z4ajBjeWxqIn0.jXJe2w2wgn8EVWmEJP15xQ'
 };
 
 /*
