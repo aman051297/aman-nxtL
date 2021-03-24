bed4016866de5a0ab9fb3b60022e6fde0cb672c8
Aman <amansaxena781@gmail.com>
2021-03-24 17:13:14 +0530

    add icon on locations

--- src/app/dashboard/home/home.component.ts
+++ src/app/dashboard/home/home.component.ts
@@ -21,7 +21,7 @@ export class HomeComponent implements AfterViewInit {
 	}
 
 	private initMap(): void {
-		this.map = L.map("map").setView([27.88, 78.08], 13);
+		this.map = L.map("map").setView([27.88, 78.08], 12);
 		doubleClickZoom: false;
 
 		// preferCanvas:true;
@@ -38,16 +38,73 @@ export class HomeComponent implements AfterViewInit {
 			{
 				maxZoom: 20,
 				minZoom: 10,
-				// id:'mapbox.outdoors',
-
 				id: "mapbox/streets-v11",
 				accessToken: environment.MAPBOX_TOKEN,
 			}
 		);
+		var datacentre = L.icon({
+			iconUrl: "assets/images/datacentre.svg",
+			iconSize: [38, 95], // size of the icon
+			iconAnchor: [22, 94], // point of the icon which will correspond to marker's location
+			shadowAnchor: [4, 62], // the same for the shadow
+			popupAnchor: [-3, -76], // point from which the popup should open relative to the iconAnchor
+		});
+		var Junction = L.icon({
+			iconUrl: "assets/images/junction.svg",
+
+			iconSize: [38, 95], // size of the icon
+			iconAnchor: [22, 94], // point of the icon which will correspond to marker's location
+			shadowAnchor: [4, 62], // the same for the shadow
+			popupAnchor: [-3, -76], // point from which the popup should open relative to the iconAnchor
+		});
+
+		var Highway = L.icon({
+			iconUrl: "assets/images/highway.svg",
+
+			iconSize: [38, 95], // size of the icon
+			iconAnchor: [22, 94], // point of the icon which will correspond to marker's location
+			shadowAnchor: [4, 62], // the same for the shadow
+			popupAnchor: [-3, -76], // point from which the popup should open relative to the iconAnchor
+		});
+
+		var Control_room = L.icon({
+			iconUrl: "assets/images/control_room.svg",
+
+			iconSize: [38, 95], // size of the icon
+			iconAnchor: [22, 94], // point of the icon which will correspond to marker's location
+			shadowAnchor: [4, 62], // the same for the shadow
+			popupAnchor: [-3, -76], // point from which the popup should open relative to the iconAnchor
+		});
+
 		tiles.addTo(this.map);
-		L.marker([27.88, 78.08])
+		L.marker([27.844444, 78.154167], { icon: Junction })
 			.addTo(this.map)
-			.bindPopup("Test Junction")
+			.bindPopup("KSM")
+			.openPopup();
+
+		L.marker([27.858889, 78.145278], { icon: Highway })
+			.addTo(this.map)
+			.bindPopup("KSM-Highway1")
+			.openPopup();
+
+		L.marker([27.851944, 78.170556], { icon: datacentre })
+			.addTo(this.map)
+			.bindPopup("KSM-DataCentre")
+			.openPopup();
+
+		L.marker([27.838611, 78.200556], { icon: Control_room })
+			.addTo(this.map)
+			.bindPopup("KSM-ControlRoom")
+			.openPopup();
+
+		L.marker([27.845556, 78.119444], { icon: Junction })
+			.addTo(this.map)
+			.bindPopup("Test_Junction")
+			.openPopup();
+
+		L.marker([27.845, 78.146667], { icon: Junction })
+			.addTo(this.map)
+			.bindPopup("Uppal")
 			.openPopup();
 	}
 
